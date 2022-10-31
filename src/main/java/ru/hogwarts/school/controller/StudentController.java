588d103ae6ffe2f2a2e207eeff3f317e7394f34c
Igor <47169805+IgorDevR@users.noreply.github.com>
2022-11-01 03:39:51 +0600

    hw3.2

---
+++ src/main/java/ru/hogwarts/school/controller/StudentController.java
@@ -0,0 +1,63 @@
+package ru.hogwarts.school.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.hogwarts.school.model.Student;
+import ru.hogwarts.school.service.StudentService;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService service) {
+        this.studentService = service;
+    }
+
+
+    @GetMapping("{studentId}")
+    public ResponseEntity getStudent(@PathVariable Long studentId) {
+        Student student = studentService.getStudentById(studentId);
+        if (student == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(student);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.createStudent(student);
+    }
+
+    @PutMapping
+    public ResponseEntity updateStudent(@RequestBody Student student) {
+        Student editStudent = studentService.updateStudent(student);
+        if (editStudent == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(editStudent);
+    }
+
+    @DeleteMapping("{studentId}")
+    public ResponseEntity deleteStudent(@PathVariable Long studentId) {
+        Student editStudent = studentService.deleteStudent(studentId);
+        if (editStudent == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(editStudent);
+    }
+
+    @GetMapping("/filterAge/{age}")
+    public ResponseEntity<List<Student>> filterStudentsForAge(@PathVariable Integer age) {
+
+        List<Student>  students= studentService.getAllStudent().stream().filter(a -> a.getAge() == age)
+                .collect(Collectors.toList());
+        if(students.size() == 0 || students.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(students);
+    }
+
+}
---
+++ src/main/java/ru/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,45 @@
+package ru.hogwarts.school.service;
+
+import org.springframework.stereotype.Service;
+import ru.hogwarts.school.model.Faculty;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class FacultyService {
+
+    private static Long idFaculty = 1l;
+    private Map<Long, Faculty> facultyMap = new HashMap<>();
+
+    public Faculty createFaculty(Faculty faculty) {
+        facultyMap.put(idFaculty++, faculty);
+        return faculty;
+    }
+
+    public Faculty getFacultyById(Long id) {
+        return facultyMap.get(id);
+    }
+
+    public Faculty updateFaculty(Faculty faculty) {
+        if (facultyMap.containsKey(faculty.getId())) {
+            facultyMap.put(faculty.getId(), faculty);
+            return faculty;
+        }
+        return null;
+    }
+
+    public Faculty deleteFaculty(Long id) {
+        if (facultyMap.containsKey(id)) {
+            return facultyMap.remove(id);
+        }
+        return null;
+    }
+
+    public List<Faculty> getAllFaculty() {
+        return facultyMap.values().stream().toList();
+    }
+
+
+}
